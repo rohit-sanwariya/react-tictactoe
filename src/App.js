41965a1915b61e4f11021ee8ae155adadae66fb7
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-02-05 14:55:57 +0530

    Board and Square.js created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Board from './components/Board';
+
 
 function App() {
   return (
-    <>
-     
-    </ >
+    <div>
+     <h1>Tic Tac Toe</h1>
+     <Board/>
+    </div>
   );
 }
 
